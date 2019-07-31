635b898d57f23d6623c4397aef463cd179ef5692
dpatel0114 <1991dharapatel@gmail.com>
2019-07-31 15:45:56 -0400

    readme

--- frontend/src/containers/Profile.js
+++ frontend/src/containers/Profile.js
@@ -11,26 +11,24 @@ export class Profile extends Component {
 
 
 
-constructor(props){
-  super(props)
-  this.state = {
-    editUser: {
+  constructor(props){
+    super(props)
+    this.state = {
+      editUser: {
 
+      }
     }
-   }
-}
+  }
 
 
 componentDidMount(){
   fetch(`http://localhost:3000/users/${localStorage.uid}`)
   .then(res => res.json())
   .then(data => 
-{this.setState({editUser: data.user})
-console.log(this.state)
-}
-
-      )
-}
+      {this.setState({editUser: data.user})
+      console.log(this.state)
+      }
+    )}
 
   handleChange = (e) => {
     this.setState({
@@ -64,17 +62,15 @@ console.log(this.state)
   }
 
 
-
   render() {
     // console.log("Profile: ", this.props.user.firstname)
     return (
       <div>
      
-    
+      <h4 class="text-center" style={{"margin-top":"1%"}}>  Welcome {this.state.editUser.firstname} !</h4>
       <div className=" card container col-md-5 shadow" style={{"margin-top":"2%"}}>
             <h5 className="card-header shadow"> <i class="fas fa-user"></i>&nbsp; Profile</h5>
             <div className="card-body text-left">
-             {/* <h5 className="card-title"></h5> */}
              <p className="card-text"> Firstname: &nbsp; &nbsp; {this.state.editUser.firstname} </p>
              <p className="card-text"> Lastname: &nbsp; &nbsp; {this.state.editUser.lastname} </p>
              <p className="card-text"> username: &nbsp; &nbsp; {this.state.editUser.username}</p>
@@ -84,7 +80,6 @@ console.log(this.state)
          </div>
 
 
-       {/* <Container> */}
         <div class="container col-md-5" style={{"margin-top":"2%"}}>
           <Form onSubmit={this.updateProfile} >
 
